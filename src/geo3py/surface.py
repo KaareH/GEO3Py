2ff1c620530529236c899101a1f55c3c6621860b
Kaare Hansen <kaare@gshansen.dk>
2022-12-13 12:36:06 +0100

    Update surface.py

--- src/geo3py/surface.py
+++ src/geo3py/surface.py
@@ -4,6 +4,8 @@ from .functions import *
 from .utils import *
 from sympy import *
 from collections import namedtuple
+from spb import plot3d_parametric_surface, PB, MB, KB
+from numpy import ptp, array, min, max
 
 class Surface3D:
     def __init__(self, name, r, dom_1, dom_2):
@@ -53,4 +55,27 @@ class Surface3D:
     
     def getMiddelH(self):
         return get_MiddelH(self.getWeingarten())
+
+    def quickPlot(self, showPlot=True):
+        plt1 = plot3d_parametric_surface(*self.r, self.dom_1, self.dom_2, backend=MB, rendering_kw={"alpha": 0.8}, wireframe=True, use_cm=False, show=False)
+        xs = plt1[0].get_data()[0]
+        ys = plt1[0].get_data()[1]
+        zs = plt1[0].get_data()[2]
+
+        ranges = array([ptp(xs), ptp(ys), ptp(zs)])
+        max_range = max(ranges)
+
+        x_ratio = max_range/ranges[0]
+        y_ratio = max_range/ranges[1]
+        z_ratio = max_range/ranges[2]
+
+        plt1.xlim = (min(xs) * x_ratio, max(xs) * x_ratio)
+        plt1.ylim = (min(ys) * y_ratio, max(ys) * y_ratio)
+        plt1.zlim = (min(zs) * z_ratio, max(zs) * z_ratio)
+
+        if showPlot:
+            plt1.show()
+            return None
+            
+        return plt1
 
